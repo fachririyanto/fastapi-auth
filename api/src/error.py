d12e0cacfbcc2fc2c1b0ce9a4d585b21cfe9dca8
Fachri Riyanto <fachri@redpumpkin.ai>
2025-12-17 16:36:19 +0700

    add: new exception

--- api/src/error.py
+++ api/src/error.py
@@ -4,7 +4,19 @@ class UnauthorizedError(Exception):
         super().__init__(*args)
 
 
+class ForbiddenError(Exception):
+    """Exception raised when user does not have permission to access a resource."""
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
 class DataNotFoundError(Exception):
     """Exception raised when requested data is not found."""
     def __init__(self, *args):
         super().__init__(*args)
+
+
+ERROR_MESSAGES = {
+    "unauthorized": "Unauthorized",
+    "forbidden": "You do not have permission to access this resource",
+}
